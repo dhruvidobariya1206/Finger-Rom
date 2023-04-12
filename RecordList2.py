6818e4c98e9dec61c79ed0585227ee80ba3afcea
Dhruvi <dhruvi.dobariya110826@marwadiuniversity.ac.in>
2023-04-12 07:50:07 +0530

    .

--- RecordList2.py
+++ RecordList2.py
@@ -21,16 +21,16 @@ import Login
 gender = ""
 
 
-class RecordList(tk.Tk):
+class RecordList2(tk.Tk):
 
     def __init__(self, *args, **kwargs):
 
         tk.Tk.__init__(self, *args, **kwargs)
 
-        self.main_frame = tk.Frame(self, bg="#9EAABF", height=700, width=800)
+        main_frame = tk.Frame(self, bg="#9EAABF", height=700, width=800)
         # pack_propagate prevents the window resizing to match the widgets
-        self.main_frame.pack_propagate(0)
-        self.main_frame.pack(fill="both", expand="true")
+        main_frame.pack_propagate(0)
+        main_frame.pack(fill="both", expand="true")
 
         self.geometry("1200x750")
         # self.resizable(0, 0)
@@ -43,51 +43,46 @@ class RecordList(tk.Tk):
 
         def only_numbers(char):
             return char.isdigit()
-        validation = self.main_frame.register(only_numbers)
+        validation = main_frame.register(only_numbers)
 
-        lb1 = Label(self.main_frame, text="List Records", width=57, height=1, font=(
+        lb1 = Label(main_frame, text="List Records", width=57, height=1, font=(
             "Verdana", 22), foreground='#FFFFFF', background="#1c4966")
         lb1.place(x=0, y=0)
 
-        Button(self.main_frame, text="Back", width=15, font=("Verdana", 14), foreground='#FFFFFF',
+        Button(main_frame, text="Back", width=15, font=("Verdana", 14), foreground='#FFFFFF',
                background="#1c4966", command=lambda: back()).place(x=1025, y=0)
 
-        lb0 = Label(self.main_frame, text="Patient Id", width=10,
+        lb0 = Label(main_frame, text="Patient Id", width=10,
                     font=("Verdana", 12), background="#9EAABF")
         lb0.place(x=0, y=60)
         options = ['C', 'J']
         clicked = StringVar()
         clicked.set(options[0])
-        drop = OptionMenu(self.main_frame, clicked, *options)
+        drop = OptionMenu(main_frame, clicked, *options)
         drop.place(x=110, y=59)
 
-        en1 = Entry(self.main_frame, width=10, validate="key",
+        en1 = Entry(main_frame, width=10, validate="key",
                     validatecommand=(validation, '%S'))
         en1.place(x=180, y=62)
 
-        lb0 = Label(self.main_frame, text="Hand", width=10,
+        lb0 = Label(main_frame, text="Hand", width=10,
                     font=("Verdana", 12), background="#9EAABF")
         lb0.place(x=0, y=120)
         options_hand = ['Left', 'Right']
         hand_value = StringVar()
         hand_value.set(options_hand[0])
-        drop2 = OptionMenu(self.main_frame, hand_value, *options_hand)
+        drop2 = OptionMenu(main_frame, hand_value, *options_hand)
         drop2.place(x=110, y=120)
         
-        Button(self.main_frame, text="Clear", width=10, command=lambda: clear(),
+        Button(main_frame, text="Clear", width=10, command=lambda: clear(),
                foreground='#FFFFFF', background="#1c4966").place(x=1000, y=62)
 
-        Button(self.main_frame, text="View", width=10, command=lambda: view(),
+        Button(main_frame, text="View", width=10, command=lambda: view(),
                foreground='#FFFFFF', background="#1c4966").place(x=200, y=120)
 
         def back():
             RecordList.destroy(self)
             Login.LoginPage()
-
-        def remove_widget(frame, widget):
-            widget.pack_forget()
-        
-        
 
         def view():
             # print("view")
@@ -108,15 +103,15 @@ class RecordList(tk.Tk):
 
                 if (row != None):
 
-                    lb1 = Label(self.main_frame, text=f"Name: {row[1]} {row[3]}", width=25, height=1, font=(
+                    lb1 = Label(main_frame, text=f"Name: {row[1]} {row[3]}", width=25, height=1, font=(
                         "Verdana", 12), foreground='#FFFFFF', background="#1c4966")
                     lb1.place(x=420, y=120)
 
-                    lb2 = Label(self.main_frame, text=f"Number: {row[4]}", width=20, height=1, font=(
+                    lb2 = Label(main_frame, text=f"Number: {row[4]}", width=20, height=1, font=(
                         "Verdana", 12), foreground='#FFFFFF', background="#1c4966")
                     lb2.place(x=700, y=120)
 
-                    lb2 = Label(self.main_frame, text=f"Age: {row[6]}", width=20, height=1, font=(
+                    lb2 = Label(main_frame, text=f"Age: {row[6]}", width=20, height=1, font=(
                         "Verdana", 12), foreground='#FFFFFF', background="#1c4966")
                     lb2.place(x=930, y=120)
 
@@ -139,43 +134,43 @@ class RecordList(tk.Tk):
                     fingers = ['index', 'middle', 'ring', 'little', 'thumb']
                     
                     for i in range(3):
-                        angle_n = Label(self.main_frame, text=(angle_names[i]), width=35, foreground='#FFFFFF', background="#1c4966").place(
+                        angle_n = Label(main_frame, text=(angle_names[i]), width=35, foreground='#FFFFFF', background="#1c4966").place(
                             x=120+(i*350), y=200)
                         for j in range(5):
-                            finger_name = Label(self.main_frame, text=fingers[j], width=5, foreground='#FFFFFF', background="#1c4966").place(
+                            finger_name = Label(main_frame, text=fingers[j], width=5, foreground='#FFFFFF', background="#1c4966").place(
                             x=120+(i*350)+(j*60), y=250)
 
                     i = 0
                     for data in dip_details:
                         # print("1")
-                        self.dip1 = Label(self.main_frame, text=str(data[0]), foreground='#FFFFFF', background="#1c4966").place(
+                        dip1 = Label(main_frame, text=str(data[0]), foreground='#FFFFFF', background="#1c4966").place(
                             x=20, y=300+(i*30))
-                        dip2=Label(self.main_frame, text=str(data[1]), width=5).place(
+                        dip2=Label(main_frame, text=str(data[1]), width=5).place(
                             x=120, y=300+(i*30))
-                        dip3=Label(self.main_frame, text=str(data[2]), width=5).place(
+                        dip3=Label(main_frame, text=str(data[2]), width=5).place(
                             x=180, y=300+(i*30))
-                        dip4=Label(self.main_frame, text=str(data[3]), width=5).place(
+                        dip4=Label(main_frame, text=str(data[3]), width=5).place(
                             x=240, y=300+(i*30))
-                        dip5=Label(self.main_frame, text=str(data[4]), width=5).place(
+                        dip5=Label(main_frame, text=str(data[4]), width=5).place(
                             x=300, y=300+(i*30))
-                        dip6=Label(self.main_frame, text=str(data[5]), width=5).place(
+                        dip6=Label(main_frame, text=str(data[5]), width=5).place(
                             x=360, y=300+(i*30))
                         i+=1
                         
                     i = 0
                     for data in pip_details:
                         # print("1")
-                        # Label(self.main_frame, text=str(data[0])).place(
+                        # Label(main_frame, text=str(data[0])).place(
                         #     x=20, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[1]), width=5).place(
+                        Label(main_frame, text=str(data[1]), width=5).place(
                             x=470, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[2]), width=5).place(
+                        Label(main_frame, text=str(data[2]), width=5).place(
                             x=530, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[3]), width=5).place(
+                        Label(main_frame, text=str(data[3]), width=5).place(
                             x=590, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[4]), width=5).place(
+                        Label(main_frame, text=str(data[4]), width=5).place(
                             x=650, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[5]), width=5).place(
+                        Label(main_frame, text=str(data[5]), width=5).place(
                             x=710, y=300+(i*30))
                         i+=1
                         
@@ -183,17 +178,17 @@ class RecordList(tk.Tk):
                     pos=0
                     for data in mcp_details:
                         print("1")
-                        # Label(self.main_frame, text=str(data[0])).place(
+                        # Label(main_frame, text=str(data[0])).place(
                         #     x=20, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[1]), width=5).place(
+                        Label(main_frame, text=str(data[1]), width=5).place(
                             x=820, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[2]), width=5).place(
+                        Label(main_frame, text=str(data[2]), width=5).place(
                             x=880, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[3]), width=5).place(
+                        Label(main_frame, text=str(data[3]), width=5).place(
                             x=940, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[4]), width=5).place(
+                        Label(main_frame, text=str(data[4]), width=5).place(
                             x=1000, y=300+(i*30))
-                        Label(self.main_frame, text=str(data[5]), width=5).place(
+                        Label(main_frame, text=str(data[5]), width=5).place(
                             x=1060, y=300+(i*30))
                         pos=300+(i*30)
                         i+=1
@@ -234,7 +229,7 @@ class RecordList(tk.Tk):
                         graph_tip.set_ylabel('Angle Measure')
                         graph_tip.set_xlabel('Progress')
                         # graph_tip.set_legend(loc="bottom right")
-                        line1 = FigureCanvasTkAgg(figure1, self.main_frame)
+                        line1 = FigureCanvasTkAgg(figure1, main_frame)
                         line1.get_tk_widget().place(x=120,y=pos+50)
                         for i in range(1,5):
                             dip[i].plot(kind='line', label=f"{lbl[i-0]}", legend=True, ax=graph_tip, marker="o")
@@ -244,7 +239,7 @@ class RecordList(tk.Tk):
                         graph_pip = figure2.add_subplot(1,1,1)
                         graph_pip.set_ylabel('Angle Measure')
                         graph_pip.set_xlabel('Progress')
-                        line1 = FigureCanvasTkAgg(figure2, self.main_frame)
+                        line1 = FigureCanvasTkAgg(figure2, main_frame)
                         line1.get_tk_widget().place(x=470,y=pos+50)
                         for i in range(1,5):
                             pip[i].plot(kind='line', label=f"{lbl[i-1]}", legend=True, ax=graph_pip, marker="o")
@@ -254,7 +249,7 @@ class RecordList(tk.Tk):
                         graph_mcp = figure3.add_subplot(1,1,1)
                         graph_mcp.set_ylabel('Angle Measure')
                         graph_mcp.set_xlabel('Progress')
-                        line1 = FigureCanvasTkAgg(figure3, self.main_frame)
+                        line1 = FigureCanvasTkAgg(figure3, main_frame)
                         line1.get_tk_widget().place(x=820,y=pos+50)
                         for i in range(1,5):
                             mcp[i].plot(kind='line', label=f"{lbl[i-1]}", legend=True, ax=graph_mcp, marker="o")
@@ -264,9 +259,7 @@ class RecordList(tk.Tk):
                 else:
                     tk.messagebox.showerror(
                         "Information", "Entered patient id is invalid")
-        
-        def clear():
-            remove_widget(self.main_frame, self.dip1)
+
 
 # root = RecordList()
 # root.title("Records Page")
